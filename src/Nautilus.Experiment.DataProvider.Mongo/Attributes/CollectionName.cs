323422fe272bfb835b43b34565f66f4be323744a
Shah Z. S <shahzshafie@outlook.com>
2021-01-14 22:02:09 +0800

    impl CollectionName attribute

---
+++ src/Nautilus.Experiment.DataProvider.Mongo/Attributes/CollectionName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nautilus.Experiment.DataProvider.Mongo.Attributes
+{
+	public class CollectionName : Attribute
+	{
+		public CollectionName(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; private set; }
+	}
+}
