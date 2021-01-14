3a653248a730b5deba8c0a96b34e9350245cbcc5
Shah Z. S <shahzshafie@outlook.com>
2021-01-14 22:05:16 +0800

    CollectionNameAttribute can only be applied on class definition

--- src/Nautilus.Experiment.DataProvider.Mongo/Attributes/CollectionName.cs
+++ src/Nautilus.Experiment.DataProvider.Mongo/Attributes/CollectionName.cs
@@ -2,6 +2,7 @@
 
 namespace Nautilus.Experiment.DataProvider.Mongo.Attributes
 {
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
 	public class CollectionName : Attribute
 	{
 		public CollectionName(string name)
