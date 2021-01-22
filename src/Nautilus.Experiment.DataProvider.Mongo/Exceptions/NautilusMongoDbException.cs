b896577bb0c5640fcd2732c5caa5428ae2a093ff
Shah Z. S <shahzshafie@outlook.com>
2021-01-22 11:27:44 +0800

    include Nautilus lib and mongo exception class

---
+++ src/Nautilus.Experiment.DataProvider.Mongo/Exceptions/NautilusMongoDbException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nautilus.Data.Exceptions;
+
+namespace Nautilus.Experiment.DataProvider.Mongo.Exceptions
+{
+    public class NautilusMongoDbException : NautilusDataExceptionBase
+    {
+        public NautilusMongoDbException()
+        {
+        }
+
+        public NautilusMongoDbException(string message) : base(message)
+        {
+        }
+
+        public NautilusMongoDbException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
