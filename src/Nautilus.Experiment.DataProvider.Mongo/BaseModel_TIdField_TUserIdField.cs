f727fbb1a7d3a6c61f3f82ccd5b1e0b98e73553c
Shah Z. S <shahzshafie@outlook.com>
2021-02-04 10:13:51 +0800

    add in [BsonId]

--- src/Nautilus.Experiment.DataProvider.Mongo/BaseModel_TIdField_TUserIdField.cs
+++ src/Nautilus.Experiment.DataProvider.Mongo/BaseModel_TIdField_TUserIdField.cs
@@ -1,8 +1,12 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Nautilus.Experiment.DataProvider.Mongo
 {
-	public class BaseModel<TIdField, TUserIdField>
+	public class MongoBaseModel<TIdField, TUserIdField>
 	{
+		[BsonId]
 		public TIdField Id { get; set; }
+
 		public TUserIdField UserId { get; set; }
 	}
 }
